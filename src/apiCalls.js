cc6e0eedfbe590f5ef90d3319fa6b60656ec2980
Thomas Fettig <thomasmfettig@gmail.com>
2022-08-01 16:03:55 -0600

    Add final touches to POST function, clean up commented code, add semi colons

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,7 @@
 const fetchAllData = (dataType) => {
     return fetch(`http://localhost:3001/api/v1/${dataType}`)
     .then(response => response.json())
-    .catch(error => console.log('API error: ${error.message}'));
+    .catch(error => console.log(`API error: ${error.message}`));
   }
 
 const getAllData = () => {
